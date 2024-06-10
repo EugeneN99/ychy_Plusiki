5307f678080bb3d4e561937c13e51258a40052d5
Neugene99 <Neugene99@yandex.ru>
2024-06-10 15:54:34 +0300

    хз че это

--- C+++++++/main.cpp
+++ C+++++++/main.cpp
@@ -1,19 +1,2 @@
-#include<iostream>
-using namespace std;
 
-
-
-void main()
-{
-	setlocale(LC_ALL, "Rus");
-	int Var, bar;
-	const char end = 'n';
-
-	cout << "¬ведите первое число:";
-	cin >> Var;
-	cout << "¬ведите втрое число";
-	cin >> bar;
-	cout << "—умма двух чисел: " << Var + bar << "\n\t" ;
-
-}
 
