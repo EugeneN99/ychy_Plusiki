f59d7eeacd4ea43f8eb8c24b16affd0538bc8940
user <user@ASY228>
2024-06-10 14:42:36 +0300

    Добавьте файлы проекта.

---
+++ C+++++++/main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+
+
+void main()
+{
+	setlocale(LC_ALL, "Rus");
+	int Var, bar;
+	const char end = 'n';
+
+	cout << "¬ведите первое число:";
+	cin >> Var;
+	cout << "¬ведите втрое число";
+	cin >> bar;
+	cout << "—умма двух чисел: " << Var + bar << "\n\t" ;
+
+}
+
