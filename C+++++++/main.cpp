d2bf28480fe9546b3feecf83410181d65905499a
Neugene99 <Neugene99@yandex.ru>
2024-06-10 18:07:17 +0300

    gg

--- C+++++++/main.cpp
+++ C+++++++/main.cpp
@@ -4,11 +4,12 @@ using namespace std;
 void main()
 {
 	setlocale(LC_ALL, "Rus");
-	int first_num = 6, second_num = 12, third_num = 18;
-	int sum, product, ar_m;
+	int first_num = 7, second_num = 12, third_num = 18;
+	int sum, product;
+	double ar_m;
 	sum = first_num + second_num + third_num;
 	product = first_num * second_num * third_num;
-	ar_m = sum / 3;
+	ar_m =  (double) sum / 3;
 	cout << "\t\tПервое число: " << first_num << "\t\tВторое число: " << second_num << "\tТретье число: " << third_num;
 	cout << "\n\t\t\tСумма чисел: " << sum << "\n\t\t\tПроизведение чисел: " << product;
 	cout << "\n\t\t\tСреднее арифметическое чисел: " << ar_m;
